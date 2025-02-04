5ed0e8c0f51a0907628ad0d2a1a3d98ce4ba73f3
MaxSuvorov <138803825+MaxSuvorov@users.noreply.github.com>
2025-02-04 15:00:03 +1000

    Add files via upload

---
+++ src/data/projects.tsx
@@ -0,0 +1,39 @@
+import { Project } from '../Types/Project.tsx';
+
+export const projects: Project[] = [
+    {
+        id: 1,
+        title: 'Calculator_by_me',
+        description: 'Да обычный калькулятор',
+        technologies: ['C++', 'OOP'],
+        link: 'https://github.com/MaxSuvorov/Calculator_by_me',
+    },
+    {
+        id: 2,
+        title: 'Why-r-u-studying-so-bad',
+        description: 'This app shows you why you have a low ranking in your university!',
+        technologies: ['C++'],
+        link: 'https://github.com/MaxSuvorov/Why-r-u-studying-so-bad',
+    },
+    {
+        id: 3,
+        title: 'webCarddd',
+        description: 'This is my personal card. I hope that now my number of subscribers will increase... maybe :/',
+        technologies: ['HTML', 'CSS'],
+        link: 'https://github.com/MaxSuvorov/webCarddd',
+    },
+    {
+        id: 4,
+        title: 'App_mile_km',
+        description: 'This windowed desktop application for converting miles to kilometers and kilometers to miles',
+        technologies: ['JavaScript' ],
+        link: 'https://github.com/MaxSuvorov/App_mile_km',
+    },
+    {
+        id: 4,
+        title: 'Recipes_blog',
+        description: 'This project aims to create a dynamic recipe publishing website',
+        technologies: ['HTML', 'CSS'],
+        link: 'https://github.com/MaxSuvorov/Recipes_blog',
+    },
+];
